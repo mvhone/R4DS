88cc477f40a41e8ab1436e9c92bea80d2e2079db
mvhone <matthew.vanhoeck@gmail.com>
2018-10-16 16:46:27 -0400

    small tweaks

--- setup.R
+++ setup.R
@@ -1,10 +1,12 @@
-## One time setup
+# One time setup
 
 # install.packages('installr')
 library(installr)
 
 updateR()
 
-require2(c('tidyverse', 'nycflights13', 'gapminder', 'Lahman'))
+require2(c("tidyverse", "nycflights13", "gapminder", "Lahman"))
 
 tidyverse_update()
+
+installr

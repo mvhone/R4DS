871b236b5c80b0bf6ed309afe695fd1d82cf93af
mvhone <matthew.vanhoeck@gmail.com>
2018-11-06 16:33:37 -0500

    Progress on Nov 6th at work

--- wrangle/Manipulation basics.R
+++ wrangle/Manipulation basics.R
@@ -57,3 +57,42 @@ eight$dep_time
 
 nine <- filter(flights, is.na(dep_time))
 nine
+
+--------------------------------------------------------------------------------
+# https://r4ds.had.co.nz/transform.html#add-new-variables-with-mutate
+
+library(tidyverse)
+library(nycflights13)
+  
+flights_sml <- select(
+  flights,
+  year:day,
+  ends_with("delay"),
+  distance,
+  air_time
+)
+mutate(flights_sml,
+  gain = dep_delay - arr_delay,
+  speed = distance / air_time * 60
+)
+
+mutate(flights_sml,
+       gain = dep_delay - arr_delay,
+       hours = air_time / 60,
+       gain_per_hour = gain / hours
+       ) 
+
+transmute(flights,
+          gain = dep_delay - arr_delay,
+          hours = air_time / 60,
+          gain_per_hour = gain / hours
+          )
+
+transmute(flights,
+          dep_time,
+          hour = dep_time %/% 100,
+          minute = dep_time %% 100
+) # breaks integers into pieces
+
+# log2() interpretation - difference of 1 corresponds to doubling, -1 halving
+
